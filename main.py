dfdbec02aa41b42dd44e6772b1c7573167aac3e5
MusaTamzid05 <tamzid101@gmail.com>
2024-02-19 23:26:23 +0600

    Add simple tag line tracker

--- main.py
+++ main.py
@@ -3,6 +3,44 @@ from lib.formatter import FileFormatter
 from lib.formatter import DirFormatter
 import os
 
+def get_tagname_from(line):
+    tag_name = line.strip().strip("<").strip(">").split()[0]
+    return tag_name
+
+
+
+def get_tag_range(html_lines, start_index):
+    tag_name = get_tagname_from(line=html_lines[start_index])
+    start_tag = "<" + tag_name  + ">"
+    end_tag = "</" + tag_name + ">"
+    print(start_tag)
+
+    current_index = start_index + 1 
+    ignore_end_tag = 0
+
+
+    for line in html_lines[start_index + 1:]:
+        line = line.strip()
+
+
+        if start_tag == line:
+            ignore_end_tag += 1
+
+        elif end_tag == line:
+            if ignore_end_tag > 0:
+                print("Ignoring end tag")
+                ignore_end_tag -= 1
+            else:
+                return current_index
+
+
+        current_index += 1
+
+    return None
+
+
+
+
 
 def main():
     parser = argparse.ArgumentParser("html parser")
@@ -20,4 +58,8 @@ def main():
 
 
 if __name__ == "__main__":
-    main()
+    start_index = 1
+    with open("test.html") as f:
+        lines = f.readlines()
+        end_index = get_tag_range(html_lines=lines, start_index=start_index)
+        print("".join(lines[start_index:end_index + 1]))
